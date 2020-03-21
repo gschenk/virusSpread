bddef0e2036f35a280cfdb16bf59aecbf484cd07
gerald schenk <gschenk@gmx.net>
2020-03-21 23:15:50 +0100

    adds parameter for initial infected

--- virus.hs
+++ virus.hs
@@ -6,14 +6,16 @@ import Text.Printf
 
 
 -- simple exponential growth, each step n is one incubation period
-simple :: Double -> Int -> Int
---simple :: (RealFrac a, Floating a, Integral b) => a -> b -> b
-simple r n = round (r^n)
+-- simple :: Double -> Int -> Int
+-- simple :: (RealFrac a, Floating a, Integral b) => a -> b -> b -> b
+simple :: Double -> Int -> Int -> Int
+simple r i n = round (r**a)
+   where a = fromIntegral n + (log (fromIntegral i)/log r)
 
 
 -- simple accumulated
-simpleAcc :: Double -> Int -> Int
-simpleAcc r = (foldl (+) 0) .map(simple r).l
+--simpleAcc :: Double -> Int -> Int
+simpleAcc r i = (foldl (+) 0) .map(simple r i).l
    where l n = [0..n]
 
 
@@ -32,7 +34,8 @@ tailDiff (_:ns) = tailDiff ns
 
 
 -- accumulated new infected from list of infected
-accFromList :: (Integral b) => Double -> b -> [b] -> b
+-- accFromList :: (Integral b) => Double -> b -> [b] -> b
+accFromList :: Double -> Int -> [Int] -> Int
 --accFromList :: (RealFrac a, Integral b) => a -> b -> [b] -> b
 accFromList _ _ [] = 0
 accFromList _ 0 is = 0
@@ -40,20 +43,20 @@ accFromList 0 _ is = last is
 accFromList rIni nTot is =  f n
    where i = tailDiff is -- number infectious cases
          r = (*rIni).(+1).(/ (fromIntegral nTot)).negate.fromIntegral.last $ is -- effective replication number
-         n = last is + round r * i -- newly infected
+         n = (+ last is) . round . (* r) . fromIntegral $ i -- newly infected
          f n
              | n > nTot = nTot
              | otherwise = n
 
 -- accumulated cases new, and old, with pop limit
 -- limitedAcc :: Integral b => Double -> b -> Int -> [b]
-limitedAcc rIni nTot n = last . take n . iterate f $ [1]
+limitedAcc rIni nTot iIni n = last . take n . iterate f $ [iIni]
     where f is = is ++ [accFromList rIni nTot is]
 
 -- new cases with population limit
-limited :: Integral b => Double -> b -> Int -> [b]
-limited rIni nTot n = map f [1..n]
-    where f = tailDiff . limitedAcc rIni nTot
+--limited :: Integral b => Double -> b -> Int -> Int -> [b]
+limited rIni nTot iIni n = map f [1..n]
+    where f = tailDiff . limitedAcc rIni nTot iIni
 
 
 -- transpose list of lists
@@ -62,12 +65,12 @@ transpose xs = (map head xs) : transpose (map tail xs)
 
 
 -- calculate results
-results r n s = ss : sim : simA : lim : limA : []
+results r n s i = ss : sim : simA : lim : limA : []
     where ss = [0..s]
-          sim = map (simple r) ss
-          simA = map (simpleAcc r) ss
-          lim = limited r n (s+1)
-          limA = limitedAcc r n (s+1)
+          sim = map (simple r i) ss
+          simA = map (simpleAcc r i) ss
+          lim = limited r n i (s+1)
+          limA = limitedAcc r n i (s+1)
 
 -- format pretty output
 pretty :: [[Int]] -> String
@@ -80,7 +83,7 @@ head1 = ["","Simple Model","","Population Limited Model"]
 head2 = ["Step","Infected", "Accumm.", "Infected", "Acc'd" ]
 
 
-parse (r:n:s:_) = [r,n,s]
+parse (r:n:s:i:_) = [r,n,s,i]
 parse _ = []
 
 exit = exitWith(ExitSuccess)
@@ -89,10 +92,10 @@ main = do
     args <- getArgs
     let pars = parse args
     if pars == []
-      then putStrLn "usage: virus <R> <N> <s>\nR replication number\nN population size\nsnumber of steps" >> exitWith(ExitFailure 1)
+      then putStrLn "usage: virus <R> <N> <s>\nR replication number\nN population size\ns number of steps\ni initial infected" >> exitWith(ExitFailure 1)
       else putStrLn $ "# R=" ++ pars !! 0 ++ ", N=" ++ pars !! 1 ++ ", s=" ++ pars !! 2
     let rIni = read . head $ pars :: Double
-    let (nTot:steps:[]) = map read . tail $ pars :: [Int]
+    let (nTot:steps:iIni:[]) = map read . tail $ pars :: [Int]
 --    let results = simple rIni steps
     let test = limited rIni nTot steps
-    putStrLn.pretty.transpose $ results rIni nTot steps
+    putStrLn.pretty.transpose $ results rIni nTot steps iIni
