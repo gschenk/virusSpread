713d32b59b46a177e0e808ddb9d5ca5e4e7c85a4
gerald schenk <gschenk@gmx.net>
2020-03-23 20:13:38 +0100

    refactors, adds HM sigs, adds parameter p0

--- virus.hs
+++ virus.hs
@@ -15,7 +15,7 @@ simple r i n = round (r**a)
 
 
 -- simple accumulated
---simpleAcc :: Double -> Int -> Int
+simpleAcc :: Double -> Int -> Int -> Int
 simpleAcc r i = (foldl (+) 0) .map(simple r i).l
    where l n = [0..n]
 
@@ -35,9 +35,7 @@ tailDiff (_:ns) = tailDiff ns
 
 
 -- accumulated new infected from list of infected
--- accFromList :: (Integral b) => Double -> b -> [b] -> b
-accFromList :: Double -> Int -> [Int] -> Int
---accFromList :: (RealFrac a, Integral b) => a -> b -> [b] -> b
+accFromList :: (RealFrac a, Integral b) => a -> b -> [b] -> b
 accFromList _ _ [] = 0
 accFromList _ 0 is = 0
 accFromList 0 _ is = last is
@@ -50,23 +48,26 @@ accFromList rIni nTot is =  f n
              | otherwise = n
 
 -- accumulated cases new, and old, with pop limit
--- limitedAcc :: Integral b => Double -> b -> Int -> [b]
+limitedAcc :: (RealFrac a, Integral b) => a -> b -> b -> Int -> [b]
 limitedAcc rIni nTot iIni n = last . take n . iterate f $ [iIni]
     where f is = is ++ [accFromList rIni nTot is]
 
 -- new cases with population limit
 --limited :: Integral b => Double -> b -> Int -> Int -> [b]
+limited :: (RealFrac a, Integral b) => a -> b -> b -> Int -> [b]
 limited rIni nTot iIni n = map f [1..n]
     where f = tailDiff . limitedAcc rIni nTot iIni
 
 
 -- transpose list of lists
+transpose :: [[a]] -> [[a]]
 transpose ([]:_) = []
 transpose xs = (map head xs) : transpose (map tail xs)
 
 
 -- calculate results
-results r n s i = ss : sim : simA : lim : limA : []
+results :: (Double, Int, Int, Int, Double) -> [[Int]]
+results (r,n,s,i,_) = ss : sim : simA : lim : limA : []
     where ss = [0..s]
           sim = map (simple r i) ss
           simA = map (simpleAcc r i) ss
@@ -74,19 +75,30 @@ results r n s i = ss : sim : simA : lim : limA : []
           limA = limitedAcc r n i (s+1)
 
 
+-- Output
 -- format pretty output
 pretty :: [[Int]] -> String
 pretty = unlines . (header head1:) . (header head2:) . map f
     where f = unwords . map (printf "%10d")
 
+header :: [String] -> String
 header = ((++)"#").unwords .  map (printf "%10s") 
 
 head1 = ["","Simple Model","","Population Limited Model"]
 head2 = ["Step","Infected", "Accumm.", "Infected", "Acc'd" ]
 
--- input comprehension
-parse (r:n:s:i:_) = [r,n,s,i]
-parse _ = []
+helpText = "usage: virus <R0> <N> <s> [<i> [<p>]] \n\
+    \R0 base replication number\n\
+    \N population size\n\ \s number of steps\n\
+    \i0 initial infected\n\ \p0 ratio of immune population"
+
+showParms :: [String] -> String
+showParms (r:n:s:[]) = showParms (r:n:s:"1":"0":[])
+showParms (r:n:s:i:[]) = showParms (r:n:s:i:"0":[])
+showParms (r:n:s:i:p:[]) = "#R0=" ++ r ++ " N=" ++ n ++ " s=" ++ s ++ " i0=" ++ i ++ " p0=" ++ p
+
+
+-- Input
 
 -- comprehend numbers with kilo, Mega, Giga suffix
 kiloToInt :: String -> Int
@@ -100,16 +112,27 @@ kiloToInt cs
           f 'G' = 1e9
           f  _ = error "Not a number."
 
-exit = exitWith(ExitSuccess)
+-- input comprehension
+parse :: [String] -> (Double,Int,Int,Int,Double) 
+parse (a:b:c:[]) = parse [a,b,c,"1","0"]
+parse (a:b:c:e:[]) = parse [a,b,c,e,"0"]
+parse (a:b:c:d:e:_) 
+    | p < 1 && p >= 0 = (r0, n0, s, i0, p)
+    | otherwise = error "Ratio of immune population not in range 0 <= P < 1"
+    where r0 = read a :: Double
+          n0 = kiloToInt b
+          s = read c :: Int
+          i0 = kiloToInt d
+          p = read e :: Double
+parse _ = error "Wrong number of arguments"
+
 
 main = do
     args <- getArgs
-    let pars = parse args
-    if pars == []
-      then putStrLn "usage: virus <R> <N> <s>\nR replication number\nN population size\ns number of steps\ni initial infected" >> exitWith(ExitFailure 1)
-      else putStrLn $ "# R=" ++ pars !! 0 ++ ", N=" ++ pars !! 1 ++ ", s=" ++ pars !! 2 ++ ", i=" ++ pars !!3
-    let rIni = read . head $ pars :: Double
-    let (nTot:steps:iIni:_) = map kiloToInt . tail $ pars :: [Int]
---    let results = simple rIni steps
-    let test = limited rIni nTot steps
-    putStrLn.pretty.transpose $ results rIni nTot steps iIni
+    --let parms = parse args
+    if args == [] || elem (head args) ["-h", "-?", "--help"]
+      then putStrLn helpText >> exitWith(ExitFailure 1)
+      else putStrLn $ showParms args
+
+    let parms = parse args
+    putStrLn.pretty.transpose $ results parms
